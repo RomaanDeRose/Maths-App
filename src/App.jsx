63b027ddd59667faf76e574fba4e1a6338c1698d
Roman De Rose <romandr37@gmail.com>
2022-04-01 21:03:20 -0300

    Maths App!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import { useEffect, useState } from "react";
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import OperationContainer from "./components/OperationContainer";
